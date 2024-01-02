029fe806980b119b52ea799fa93879bd52afe616
Mohammed Nowfal <134911830+nowfie@users.noreply.github.com>
2024-01-02 12:41:18 +0530

    success

--- trices_project/settings.py
+++ trices_project/settings.py
@@ -117,15 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-# settings.py
-
-# Add this line
-STATIC_ROOT = "/static"
-
-import os
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS =[
+    BASE_DIR / 'static'
+]
 MEDIA_URL = "/images/"
 MEDIA_ROOT=BASE_DIR / "static"
 # Default primary key field type
