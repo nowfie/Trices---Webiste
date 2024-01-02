12c9cc93f6fb27b49bcaa2a7d1189c8c30ff8a00
Mohammed Nowfal <134911830+nowfie@users.noreply.github.com>
2024-01-02 12:22:14 +0530

    success

--- trices_project/settings.py
+++ trices_project/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# settings.py
+
+# Add this line
+STATIC_ROOT = "/static"
+
 
 STATIC_URL = "static/"
 STATICFILES_DIRS =[
