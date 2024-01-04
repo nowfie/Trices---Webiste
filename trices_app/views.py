77b6d275380e3986827c03c3a84c6dfe833b2bf6
Mohammed Nowfal <134911830+nowfie@users.noreply.github.com>
2024-01-04 10:54:03 +0530

    Installing

--- trices_app/views.py
+++ trices_app/views.py
@@ -8,36 +8,42 @@ def list_of_services():
             'icon': '<i class="fa-solid fa-microchip p-lg-4 p-3"></i>',
             'heading':'Powering Your Business Potential through Intelligence',
             'description': 'Harness the potential of Artificial Intelligence to transform data into insights, driving innovation and intelligent decision-making.',
+            'features':['Smart Recommendations','Language Processing','Intelligent Automation','Image Processing']
             #'image':'ai.jpg'
         },
         'Data Science & Analytics': {
             'icon': '<i class="fa-solid fa-magnifying-glass-chart p-lg-4 p-3"></i>',
             'heading':'Empowering Decisions through Data Insights',
             'description': 'Unlock the power of Data Science and Analytics services, turning information into actionable insights to fuel strategic growth.',
+            'features':['Advanced Analytics','Real-time Data Updates','Custom Reporting','Accurate Prediction']
             #'image':'ds.jpg'
         },
         'Web Application': {
             'icon': '<i class="fa-solid fa-code p-lg-4 p-3"></i>',
             'heading':'Enriching Experiences, One App at a Time',
             'description': 'Dynamic and user-centric web applications that seamlessly blend functionality and design, delivering an exceptional online experience.',
+            'features':['Responsive Design','User Authentication','Security Measures','API Setup']
             #'image':'web.jpg'
         },
         'Mobile Application': {
             'icon': '<i class="fa fa-mobile p-lg-4 p-3" aria-hidden="true"></i>',
             'heading':'Enriching Experiences, One App at a Time',
             'description': 'Elevate your business with our Mobile App Development expertise, creating intuitive and feature-rich applications for a mobile-first world.',
+            'features':['Mobile Responsiveness','Offline Support','Push Notifications','Specific Functionality']
             #'image':'mobile.jpg'
         },
         'Social Media Marketing': {
             'icon': '<i class="fa-solid fa-chart-simple p-lg-4 p-3"></i>',
             'heading':'Maximizing Online Potential through SEO Mastery',
             'description': 'Maximize your online presence and engagement through our Digital Marketing strategies,and connecting your brand with the right audience.',
+            'features':['Social Media Integration','Social Media Analytics','Social Sharing','Scheduled Posts']
             #'image':'marketing.jpg'
         },
         'Search Engine Optimization': {
             'icon': '<i class="fas fa-business-time p-lg-4 p-3"></i>',
             'heading':'Navigating the Future of Social Media Marketing',
             'description': 'Optimizing a website service for SEO enhances online visibility and attracts more relevant users through strategic content and structural improvements',
+            'features':['Search-Friendly URL Structure','Optimized Content','Mobile Optimization','Site Speed and Performance']
             #'image':'ai.jpg'
         }
     }
@@ -45,15 +51,15 @@ def list_of_services():
 def team_members():
     team = {
         'Mohammed Nowfal': {
-            'role':'Team Leader',
+            'role':'EXECUTIVE OFFICER',
             'image': 'person1.png'              
         },
         'Venketakrishnan': {
-            'role':'Data Scientist',
+            'role':'DATA SCIENTIST',
             'image': 'person1.png'              
         },
         'Wasim hussain': {
-            'role':'Marketing Lead',
+            'role':'MARKETING LEAD',
             'image': 'person1.png'              
         }
     }
